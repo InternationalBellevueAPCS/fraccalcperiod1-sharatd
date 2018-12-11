fe1f553fc53b9564d1e65b674212e510d2b6a83b
s-dhananjayas <s-dhananjayas@49S50029237.it.bsd405.org>
2018-12-11 07:48:07 -0800

    Checkpoint 3

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -17,8 +17,9 @@ public class FracCalc {
     	}
     	while (!(str_input.equals("EXIT"))) {
     	*/
-    	String secondoperand = produceAnswer(str_input);
-    	System.out.println(secondoperand);
+    	String fraction = produceAnswer(str_input);
+    	System.out.println(fraction);
+    	//firstOperand(str_input);
     	//Calls produceAnswer and prints result
     		//System.out.println(secondoperand);
     		/*System.out.println("Please enter an equation using fractions if needed. Make sure to put underscore instead of space between "
@@ -39,23 +40,33 @@ public class FracCalc {
      * @return the result of the fraction after it has been calculated.
      *      Example: return ==> "1_1/4"
      */
-    	    	
+    public static String operator(String userinput) {
+    	String operator = userinput.substring(userinput.indexOf(" ")+1, userinput.lastIndexOf(" "));
+    	return operator;
+    }
+  
     public static String produceAnswer(String userinput) {
+    	String firststring = userinput.substring(0, userinput.indexOf(" "));
+    	String firstwhole = " ";
+    	String firstnumerator = " ";
+    	String firstdenominator = " ";
     	String secondstring = userinput.substring(userinput.lastIndexOf(" ")+1);
-    	String secondwhole;
-    	String secondnumerator;
-    	String seconddenominator;
+    	String secondwhole = " ";
+    	String secondnumerator = " ";
+    	String seconddenominator = " ";
+    	String output_str = " ";
     	//Declares variables for second operand
     	if (!secondstring.contains("_")) {
     		if (secondstring.contains("/")) {
         	secondnumerator = secondstring.substring(secondstring.indexOf(" ")+1, secondstring.indexOf("/"));
         	seconddenominator = secondstring.substring(secondstring.lastIndexOf("/")+1);
-        	return ("whole:0" + " " + "numerator:" + secondnumerator + " " + "denominator:" + seconddenominator);	
+       
+        	//return ("whole:0" + " " + "numerator:" + secondnumerator + " " + "denominator:" + seconddenominator);	
     		//Numerator is number before sign and denominator is number after, returns whole number, numerator, and denominator
     		}
         	else {
         	secondwhole = secondstring;
-        	return ("whole:" + secondwhole + " " + "numerator:0 denominator:1");
+        	//return ("whole:" + secondwhole + " " + "numerator:0 denominator:1");
         	//returns whole number and default numerator as well as denominator if second operand is not mixed number
         	}
     	}	
@@ -64,11 +75,182 @@ public class FracCalc {
     		System.out.println(secondwhole);
     		secondnumerator = secondstring.substring(secondstring.indexOf("_")+1, secondstring.indexOf("/"));
     		seconddenominator = secondstring.substring(secondstring.indexOf("/")+1);
-        	return ("whole:" + secondwhole + " " + "numerator:" + secondnumerator + " " + "denominator:" + seconddenominator);
+
+        	//return ("whole:" + secondwhole + " " + "numerator:" + secondnumerator + " " + "denominator:" + seconddenominator);
     	//The numerator is the number before the operator and the denominator is the number after if there is no whole number
     	//If there is an underscore, print out whole number before underscore, numerator before operator, and denominator after
     	}
-    }
+    	System.out.println("second:"+ secondnumerator + seconddenominator);
+    	System.out.println("first string:"+firststring);
+    	if (!firststring.contains("_")) {
+    		if (firststring.contains("/")) {
+    			firstnumerator = firststring.substring(firststring.indexOf(" ")+1, firststring.indexOf("/"));
+    			firstnumerator = firststring.substring(firststring.indexOf(" ")+1, firststring.indexOf("/"));
+            	firstdenominator = firststring.substring(firststring.lastIndexOf("/")+1);
+            	//return ("whole:0" + " " + "numerator:" + firstnumerator + " " + "denominator:" + firstdenominator);	
+        		//Numerator is number before sign and denominator is number after, returns whole number, numerator, and denominator
+        		
+    			}
+            	else {
+            	firstwhole = firststring;
+            	//return ("whole:" + firstwhole + " " + "numerator:0 denominator:1");
+            	//returns whole number and default numerator as well as denominator if second operand is not mixed number
+            	}
+        	}	
+        	else {
+        		firstwhole = firststring.substring(firststring.indexOf(" ")+1, firststring.indexOf("_"));
+        		System.out.println(firstwhole);
+        		firstnumerator = firststring.substring(firststring.indexOf("_")+1, firststring.indexOf("/"));
+        		firstdenominator = firststring.substring(firststring.indexOf("/")+1);
+            	//return ("whole:" + firstwhole + " " + "numerator:" + firstnumerator + " " + "denominator:" + firstdenominator);
+        	//The numerator is the number before the operator and the denominator is the number after if there is no whole number
+        	//If there is an underscore, print out whole number before underscore, numerator before operator, and denominator after
+        	
+    		}
+    	System.out.println("first:"+ firstnumerator + firstdenominator);
+    	int lcm = leastCommonMultiple(Integer.parseInt(firstdenominator), Integer.parseInt(seconddenominator));
+		//6_5/7 + 5_2/3
+    	System.out.println("firstdenominator:" + firstdenominator);
+		System.out.println("seconddenominator:" + seconddenominator);
+		System.out.println(lcm);
+    	String sign = operator(userinput);
+    	System.out.println(sign);
+    	if (sign.contains("+")) {
+    		//System.out.println(sign);
+    		if (userinput.contains("_")) {
+    			int wholenumber = Integer.parseInt(firstwhole) + Integer.parseInt(secondwhole);
+    			int num = Integer.parseInt(firstnumerator) * (lcm/Integer.parseInt(firstdenominator)) + Integer.parseInt(secondnumerator) * (lcm/Integer.parseInt(seconddenominator)); 
+    			int denominator = lcm;
+    		//System.out.println("wholenumber" + wholenumber);
+    		//System.out.println("numerator" + num);
+    		//System.out.println("denominator" + lcm);
+    		return (wholenumber + "_" + num + "/" + denominator);
+    		}
+    		else {
+    			int num = Integer.parseInt(firstnumerator) * (lcm/Integer.parseInt(firstdenominator)) + Integer.parseInt(secondnumerator) * (lcm/Integer.parseInt(seconddenominator)); 
+    			int denominator = lcm;
+    			//System.out.println("wholenumber" + wholenumber);
+        		//System.out.println("numerator" + num);
+        		//System.out.println("denominator" + lcm);
+        		return (num + "/" + denominator);
+        	}
+    		
+    	}
+    	else if (sign.contains("-")) {
+    		if (userinput.contains("_")) {
+    			int wholenumber = Integer.parseInt(firstwhole) - Integer.parseInt(secondwhole);
+    			int num = Integer.parseInt(firstnumerator) * (lcm/Integer.parseInt(firstdenominator)) - Integer.parseInt(secondnumerator) * (lcm/Integer.parseInt(seconddenominator)); 
+    			int denominator = lcm;
+    		//System.out.println("wholenumber" + wholenumber);
+    		//System.out.println("numerator" + num);
+    		//System.out.println("denominator" + lcm);
+    		
+    			return (wholenumber + "_" + num + "/" + denominator);
+    		}
+    		else {
+    			int num = Integer.parseInt(firstnumerator) * (lcm/Integer.parseInt(firstdenominator)) - Integer.parseInt(secondnumerator) * (lcm/Integer.parseInt(seconddenominator)); 
+    			int denominator = lcm;
+    			//System.out.println("wholenumber" + wholenumber);
+        		//System.out.println("numerator" + num);
+        		//System.out.println("denominator" + lcm);
+        		return (num + "/" + denominator);
+        
+    		}
+    	}
+    	else if (sign.contains("*")) {
+    		if (userinput.contains("_")) {
+    			int firstnum = Integer.parseInt(firstwhole) * Integer.parseInt(firstdenominator) + Integer.parseInt(firstnumerator);
+    			int secondnum = Integer.parseInt(secondwhole) * Integer.parseInt(seconddenominator) + Integer.parseInt(secondnumerator);
+    			int tempnum = firstnum * secondnum;
+    			int denominator = Integer.parseInt(firstdenominator) * Integer.parseInt(seconddenominator);
+    			int whole = 0;
+    			int num = 0;
+    			if (tempnum > denominator){
+    				whole = tempnum / denominator;
+    				num = tempnum % denominator;
+    			}
+    			else {
+    				num = tempnum;
+    			}
+    		//System.out.println(firstnum);
+    		//System.out.println(secondnum);
+    		//System.out.println(denominator);
+    		//System.out.println(whole);
+    		//System.out.println(num);
+    			return (whole + "_" + num + "/" + denominator);
+    		}
+    		else {
+    			int firstnum = Integer.parseInt(firstnumerator);
+    			int secondnum =  Integer.parseInt(secondnumerator);
+    			int tempnum = firstnum * secondnum;
+    			int denominator = Integer.parseInt(firstdenominator) * Integer.parseInt(seconddenominator);
+    			int whole = 0;
+    			int num = 0;
+    			if (tempnum > denominator){
+    				whole = tempnum / denominator;
+    				num = tempnum % denominator;
+    			}
+    			else {
+    				num = tempnum;
+    			}
+    			//System.out.println(firstnum);
+    			//System.out.println(secondnum);
+    			//System.out.println(denominator);
+    			//System.out.println(whole);
+    			//System.out.println(num);
+    			return (whole + "_" + num + "/" + denominator);
+    		}
+    	}
+    	else {
+    		if (userinput.contains("_")) {
+    			int firstnum = Integer.parseInt(firstwhole) * Integer.parseInt(firstdenominator) + Integer.parseInt(firstnumerator);
+    			int secondnum = Integer.parseInt(secondwhole) * Integer.parseInt(seconddenominator) + Integer.parseInt(secondnumerator);
+    			int tempnum = firstnum * Integer.parseInt(seconddenominator);
+    			int denominator = Integer.parseInt(firstdenominator) * secondnum;
+    			int whole = 0;
+    			int num = 0;
+    			if (tempnum > denominator){
+    			whole = tempnum / denominator;
+    			num = tempnum % denominator;
+    			}
+    			else {
+    				num = tempnum;
+    			}
+    		//System.out.println(firstnum);
+    		//System.out.println(secondnum);
+    		//System.out.println(denominator);
+    		//System.out.println(whole);
+    		//System.out.println(num);
+    		
+    		return (whole + "_" + num + "/" + denominator);
+    		}
+    		else {
+    			int firstnum =   Integer.parseInt(firstnumerator);
+    			int secondnum =  Integer.parseInt(secondnumerator);
+    			int tempnum = firstnum * Integer.parseInt(seconddenominator);
+    			int denominator = Integer.parseInt(firstdenominator) * secondnum;
+    			int whole = 0;
+    			int num = 0;
+    			if (tempnum > denominator){
+    			whole = tempnum / denominator;
+    			num = tempnum % denominator;
+    			}
+    			else {
+    				num = tempnum;
+    			}
+    		//System.out.println(firstnum);
+    		//System.out.println(secondnum);
+    		//System.out.println(denominator);
+    		//System.out.println(whole);
+    		//System.out.println(num);
+    		
+    		return (whole + "_" + num + "/" + denominator);
+    		}
+    	}
+    		
+    	}
+    	
+    
     	/*String firstnumerator = userinput.substring(userinput.indexOf("_") + 1, userinput.indexOf("/"));
     	String firstdenominator = userinput.substring(userinput.indexOf("/") + 1, userinput.indexOf(" "));
     	int finaldenominator = leastCommonMultiple(Integer.parseInt(firstdenominator), Integer.parseInt(seconddenominator));
